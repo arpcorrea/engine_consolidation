856ed573f179325974bed7d383f008377aaa4d06
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-11 11:32:56 -0400

    Update solves.py
    separated convection from conduction

--- consolidate/solver/solves.py
+++ consolidate/solver/solves.py
@@ -2,13 +2,13 @@
 import numpy as np
 
 
-class SolvesTwoPlates:
+class SolvesOnePlates:
     
-    def __init__(self, deck,model_HT,meshes,plots):
+    def __init__(self, deck,meshes,BC,model_HT,plots):
         self.deck = deck
-        # Compute the TEMPERATURE for each node
-        self.model_HT = model_HT
-        self.meshes=meshes  
+        self.meshes=meshes
+        self.BC=BC
+        self.model_HT=model_HT
         self.plots = plots
         self.do_solver()
         
@@ -20,15 +20,17 @@ class SolvesTwoPlates:
         for m in range(int(self.deck.doc["Simulation"]["Number Time Steps"])):
 # -------------- CALCULATE TEMPERATURE FOR EACH STEP INCREMENT----------             
             # self.A=self.model_HT.convection(self.meshes.T0, self.meshes.T, self.meshes.DiffTotalX, self.meshes.DiffTotalY)
-            self.meshes.T0, self.meshes.T = self.model_HT.do_timestep(self.meshes.T0, self.meshes.T, self.meshes.DiffTotalX, self.meshes.DiffTotalY, self.meshes.Q)
-# -------------- FORCE TEMPERATURE AT THE INTERFACE: ISOTHERMAL CONDITION----------             
+            self.BC.T0, self.BC.T = self.model_HT.do_convection(self.BC.T0, self.BC.T, self.BC.Dx, self.BC.Dy, self.BC.Q)            
+            self.BC.T0, self.BC.T = self.model_HT.do_timestep(self.BC.T0, self.BC.T, self.BC.Dx, self.BC.Dy, self.BC.Q)
+            
+            # -------------- FORCE TEMPERATURE AT THE INTERFACE: ISOTHERMAL CONDITION----------             
 # -------------- UPDATE T0----------             
-            self.meshes.T0=self.meshes.T.copy()
+            self.BC.T0=self.BC.T.copy()
 
 # -------------- DO PLOT ACCORDING TO THE SELECTED INTERVAL----------             
             if m in self.plots.mfig:
                 
-                self.plots.update_T(self.meshes.T)       
+                self.plots.update_T(self.BC.T)       
                 self.plots.do_plots(m)    
         self.plots.do_animation()
 
