f4b84bdec32fcce9947e25d34decd192a366c554
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-12 18:49:11 -0400

    Update heat_transfer.py

--- consolidate/models/heat_transfer.py
+++ consolidate/models/heat_transfer.py
@@ -12,7 +12,7 @@ class HeatTransfer:
         self.h= float(deck.doc["Boundary Condition"]["Convective Coefficient"])
 # -------------- BEGIN HEAT TRANSFER CALCULATION---------- 
 
-    def do_convection(self, u0, u, Diffx, Diffy,Q):
+    def do_convection(self, u0, u, Diffx, Diffy):
         # Propagate with forward-difference in time, central-difference in space        
 
         Q=np.zeros(np.shape(self.BC.T))
@@ -31,7 +31,8 @@ class HeatTransfer:
         
         valueX=np.zeros(np.shape(self.BC.T))
         valueX[:,0 ]  = -Q[:,0]/ self.BC.Kx[:,0]
-        valueX[:,-1]  = -Q[:,-1]/self.BC.Kx[:,-1]        
+        valueX[:,-1]  = -Q[:,-1]/self.BC.Kx[:,-1]   
+        
         valueX[0,0]   = -Q[0,0]/self.BC.Kx[0,0]     
         valueX[-1,0]  = -Q[-1,0]/self.BC.Kx[-1,0]  
         valueX[0,-1]  = -Q[0,-1]/self.BC.Kx[0,-1]   
@@ -47,14 +48,13 @@ class HeatTransfer:
         valueY[-1,0]  = -Q[-1,0]/self.BC.Ky[-1,0]
         valueY[0,-1]  = -Q[0,-1]/self.BC.Ky[0,-1]
         valueY[-1,-1] = -Q[-1,-1]/self.BC.Ky[-1,-1]       
-        self.valueY=valueY    
+        self.valueY=valueY 
         
         
         
         
         
-        UoutX=np.zeros(np.shape(self.BC.T))
-     
+        UoutX=np.zeros(np.shape(self.BC.T))     
         UoutX[:,-1]   = u[:,-1]  -2*self.Mdx[:,-1]*valueX[:,-1]
         UoutX[:,0]    = u[:,0]   -2*self.Mdx[:,0] *valueX[:,0]
         
@@ -81,17 +81,15 @@ class HeatTransfer:
         
         u[0,1:-1]     = u0[0,1:-1]     + Diffy[0,1:-1] *self.dt*      ((u0[1,1:-1]    -2*u[0,1:-1]      + UoutY[0,1:-1]) /self.dy2[0,1:-1])      + Diffx[0,1:-1] *self.dt*       ((u0[0, 2:]    -2*u0[0,1:-1]     +u0[0,:-2])/self.dx2[0,1:-1]) 
         u[-1,1:-1]    = u0[-1,1:-1]    + Diffy[-1,1:-1]*self.dt*      ((u0[-2,1:-1]   -2*u[-1,1:-1]     + UoutY[-1,1:-1])/self.dy2[-1,1:-1])     + Diffx[-1,1:-1]*self.dt*       ((u0[-1, 2:]   -2*u0[-1,1:-1]    +u0[-1,:-2])/self.dx2[-1,1:-1]) 
-        # u[1:-1, 1:-1] = u0[1:-1, 1:-1] + Diffy[1:-1, 1:-1]* self.dt * ((u0[2:, 1:-1] - 2*u0[1:-1, 1:-1] + u0[:-2, 1:-1])/self.dy2[1:-1, 1:-1] ) + Diffx[1:-1, 1:-1]* self.dt *  ((u0[1:-1, 2:] -2*u0[1:-1, 1:-1] +u0[1:-1, :-2])/self.dx2[1:-1, 1:-1] )
         
         u[1:-1,0]     = u0[1:-1,0]     + Diffy[1:-1,0] *self.dt*      ((u0[2:,0 ]     -2*u[1:-1,0]      + u0[0:-2,0]) /self.dy2[1:-1,0])       + Diffx[1:-1, 0]*self.dt*       ((u0[1:-1, 1 ] -2*u0[1:-1,0]     +UoutX[1:-1,0])/self.dx2[1:-1,0])        
         u[1:-1,-1]    = u0[1:-1,-1]    + Diffy[1:-1,-1]*self.dt*      ((u0[2:,-1]     -2*u[1:-1,-1]     + u0[0:-2,-1])/self.dy2[1:-1,-1])      + Diffx[1:-1,-1]*self.dt*       ((u0[1:-1, -2 ]-2*u0[1:-1,-1]    +UoutX[1:-1,-1])/self.dx2[1:-1,-1]) 
-        # u[1:-1, 1:-1] = u0[1:-1, 1:-1] + Diffy[1:-1, 1:-1]* self.dt * ((u0[2:, 1:-1] - 2*u0[1:-1, 1:-1] + u0[:-2, 1:-1])/self.dy2[1:-1, 1:-1] ) + Diffx[1:-1, 1:-1]* self.dt *  ((u0[1:-1, 2:] -2*u0[1:-1, 1:-1] +u0[1:-1, :-2])/self.dx2[1:-1, 1:-1] )
 
         u[0,0]   = u0[0,0]      +Diffy[0,0]      *self.dt*((u0[1,0]   -2*u[0,0]    + UoutX[0,0])/self.dy2[0,0])         +Diffx[0,0]     *self.dt*((u0[0, 1]     -2*u0[0,0]    +UoutY[0,0])/self.dx2[0,0])
         u[-1,0]  = u0[-1,0]     +Diffy[-1,0]     *self.dt*((u0[-2,0]  -2*u[-1,0]   + UoutX[-1,0])/self.dy2[-1,0])       +Diffx[-1,0]    *self.dt*((u0[-1, 1]    -2*u0[-1,0]   +UoutY[-1,0])/self.dx2[-1,0])  
         u[0,-1]  = u0[0,-1]     +Diffy[0,-1]     *self.dt*((u0[1,-1]  -2*u[0,-1]   + UoutX[0,-1])/self.dy2[0,-1])       +Diffx[0,-1]    *self.dt*((u0[0, -2]    -2*u0[0,-1]   +UoutY[0,-1])/self.dx2[0,-1])
         u[-1,-1] = u0[-1,-1]    +Diffy[-1,-1]    *self.dt*((u0[-2,-1] -2*u[-1,-1]  + UoutX[-1,-1])/self.dy2[-1,-1])     +Diffx[-1,-1]   *self.dt*((u0[-1,-2]    -2*u0[-1,-1]  +UoutY[-1,-1])/self.dx2[-1,-1])
-
+        
         u0=u.copy()
         
         return u0,u      
