b08588fc64225abacccafd5fb27426bafae4cdd3
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-12 17:18:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,16 @@ from consolidate import *
 cwd = os.getcwd()
 # All the data from deck.yaml is now in the following deck variable
 
-deck = Deck( cwd + "/deck_printing3D.yaml" )
-# deck = Deck( cwd + "/TwoPlates.yaml" )
-
+deck = Deck( cwd + "/AL_problem.yaml" )
 
 geometry = Geometry(deck)
 
-meshes = Meshes( deck,geometry )
+meshes = MeshOnePlate( deck,geometry )
 
+BC = BoundaryCondition(deck, geometry, meshes)
 
-model_HT= HeatTransfer(deck,meshes)
+model_HT= HeatTransfer(deck,meshes, BC)
 
-# model_IC = IntimateContact(meshes,deck)
+plots=PlotsTwoPlates(deck,meshes,BC)
 
-# plots=PlotsTwoPlates(deck,meshes,meshes.T,meshes.Dic)
-
-# solves = SolvesTwoPlates( deck,model_HT,meshes,plots,model_IC)
-
+solves = SolvesOnePlates( deck, meshes, BC,model_HT,plots)
