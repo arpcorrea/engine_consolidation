d9244ff2874d2a91d749b6183cdb080304041801
AndreCorrea92 <63312361+AndreCorrea92@users.noreply.github.com>
2020-08-12 18:56:20 -0400

    Update solves.py
    for convection it is not needed the Power Input Density as an argument

--- consolidate/solver/solves.py
+++ consolidate/solver/solves.py
@@ -21,7 +21,7 @@ class SolvesOnePlates:
 # -------------- CALCULATE TEMPERATURE FOR EACH STEP INCREMENT----------             
             # self.A=self.model_HT.convection(self.meshes.T0, self.meshes.T, self.meshes.DiffTotalX, self.meshes.DiffTotalY)
             self.BC.T0, self.BC.T = self.model_HT.do_convection(self.BC.T0, self.BC.T, self.BC.Dx, self.BC.Dy)            
-            self.BC.T0, self.BC.T = self.model_HT.do_timestep(self.BC.T0, self.BC.T, self.BC.Dx, self.BC.Dy, self.BC.Q)
+            self.BC.T0, self.BC.T = self.model_HT.do_timestep(self.BC.T0, self.BC.T, self.BC.Dx, self.BC.Dy)
             
             # -------------- FORCE TEMPERATURE AT THE INTERFACE: ISOTHERMAL CONDITION----------             
 # -------------- UPDATE T0----------             
